2fe58ffded3f387340802c3d60ebf60ae8a48aa4
Devendra Singh <devendrasingh20025@gmail.com>
2025-12-25 19:09:57 +0530

    Update userRoute

--- Routes/notificationRoute.js
+++ Routes/notificationRoute.js
@@ -14,5 +14,31 @@
     res.json(notifications);
     });
 
+
+
+    // PATCH /notifications/:id/read
+notificationRoute.patch(
+  "/:id/read",
+  authMiddleware(["user"]),
+  async (req, res) => {
+    try {
+      const notificationId = req.params.id;
+
+      await NotificationModel.findByIdAndUpdate(
+        notificationId,
+        { isRead: true }
+      );
+
+      res.json({
+        success: true,
+        message: "Notification marked as read",
+      });
+    } catch (err) {
+      res.status(500).json({ success: false, message: "Server error" });
+    }
+  }
+);
+
+
     module.exports = notificationRoute;
 
